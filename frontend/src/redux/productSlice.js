348108ac0c9c7a4817b03804ab915b901476d2a4
Abasay <abdulsalamasheem@gmail.com>
2024-01-10 16:43:09 +0100

    Implememnted the addToCART Logic with increase and decrease of the quantity

--- frontend/src/redux/productSlice.js
+++ frontend/src/redux/productSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { toastFunction } from '../utility/toastFunction';
 
 const initialState = {
   productList: [],
+  cartItem: [],
 };
 
 const productSlice = createSlice({
@@ -13,9 +15,58 @@ const productSlice = createSlice({
       state.productList = [...action.payload.products];
       return state;
     },
+    addCartItem: (state, action) => {
+      const check = state.cartItem.some((el) => el._id === action.payload._id);
+      if (check) {
+        toastFunction('error', 'Item  already in Cart');
+      } else {
+        toastFunction('success', 'Item added successfully');
+        const total = action.payload.price;
+        state.cartItem = [
+          ...state.cartItem,
+          { ...action.payload, qty: 1, total: total },
+        ];
+      }
+    },
+    deleteCartItem: (state, action) => {
+      toastFunction('success', 'Item Deleted');
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      state.cartItem.splice(index, 1);
+      console.log(index);
+    },
+    increaseQty: (state, action) => {
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      let qty = state.cartItem[index].qty;
+      const qtyInc = ++qty;
+      state.cartItem[index].qty = qtyInc;
+
+      const price = state.cartItem[index].price;
+      const total = price * qtyInc;
+
+      state.cartItem[index].total = total;
+    },
+    decreaseQty: (state, action) => {
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      let qty = state.cartItem[index].qty;
+      if (qty > 1) {
+        const qtyDec = --qty;
+        state.cartItem[index].qty = qtyDec;
+
+        const price = state.cartItem[index].price;
+        const total = price * qtyDec;
+
+        state.cartItem[index].total = total;
+      }
+    },
   },
 });
 
-export const { setProducts } = productSlice.actions;
+export const {
+  setProducts,
+  addCartItem,
+  increaseQty,
+  decreaseQty,
+  deleteCartItem,
+} = productSlice.actions;
 
 export default productSlice.reducer;
